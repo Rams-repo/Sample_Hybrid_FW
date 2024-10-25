e0dc180701e03c06bce31f5354d45267c99c8246
Ram Sundar <ram.sundar0016@gmail.com>
2024-10-25 12:12:56 -0400

    Commit

--- Hybrid_FW/Features/Pages/BasePage.py
+++ Hybrid_FW/Features/Pages/BasePage.py
@@ -9,7 +9,7 @@ class BasePage:
     def __init__(self, driver):
         self.driver = driver
 
-    def find_element(self, by, value, timeout=10):
+    def find_element(self, by, value, timeout=2):
         try:
             return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((by, value)))
         except TimeoutException:
--- Hybrid_FW/Features/Pages/LoginPOM.py
+++ Hybrid_FW/Features/Pages/LoginPOM.py
@@ -14,22 +14,17 @@ class LoginPage(BasePage):
     login_btn_xpath = "//input[@value='Login']"
     warning_msg_xpath = "//div[@id='account-login']/div[1]"
 
-    def verify_login_page_title(self,expected_text):
+    def verify_login_page_title(self, expected_text):
         self.retrieved_element_text_equals("login_page_title_xpath", self.login_page_title_xpath, expected_text)
 
     def enter_username(self, user_name):
-        username_field = self.find_element(By.ID, self.username_xpath)
-        if username_field:
-            username_field.send_keys(user_name)
+        self.type_into_element("username_xpath", self.username_xpath, user_name)
 
     def enter_password(self, password):
-        password_field = self.find_element(By.ID, self.password_xpath)
-        if password_field:
-            password_field.send_keys(password)
-        self.type_into_element("password_xpath",self.password_xpath,password)
+        self.type_into_element("password_xpath", self.password_xpath, password)
 
     def click_login_button(self):
         self.click_on_element("login_btn_xpath", self.login_btn_xpath)
 
-    def display_status_warning(self,expected_text):
+    def display_status_warning(self, expected_text):
         self.retrieved_element_text_equals("warning_msg_xpath", self.warning_msg_xpath, expected_text)
